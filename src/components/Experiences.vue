f9fc0bcae56403b386180399746b07fe6e733a7f
Diego Vargas <dialvaro30@gmail.com>
2022-11-25 16:06:05 -0500

    add more info skills

--- src/components/Experiences.vue
+++ src/components/Experiences.vue
@@ -1,43 +1,60 @@
 <template>
   <h4
-    class="mb-4 text-center text-xl font-medium uppercase text-blue-600 md:text-left md:text-2xl dark:text-white dark:underline transition-colors duration-200">
+    class="mb-4 text-center text-xl font-medium uppercase text-blue-600 transition-colors duration-200 dark:text-white dark:underline md:text-left md:text-2xl"
+  >
     experiencia profesional
   </h4>
-  <ol class="relative border-l border-gray-200 dark:border-gray-700 transition-colors duration-200">
+  <ol class="relative border-l border-gray-200 transition-colors duration-200 dark:border-gray-700">
     <li class="mb-10 ml-4" v-for="(item, index) in experiences" :key="index">
       <div
-        class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700 transition-colors duration-200">
-      </div>
-      <a class="text-xl font-extrabold text-gray-900 dark:text-white flex items-center transition-colors duration-200" :href="item.url">
-        <img :src="item.image" :alt="item.companyimage" class="mr-3 w-12 h-auto rounded-full"> 
+        class="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 transition-colors duration-200 dark:border-gray-900 dark:bg-gray-700"
+      ></div>
+      <a
+        class="flex items-center text-xl font-extrabold text-gray-900 transition-colors duration-200 dark:text-white"
+        :href="item.url"
+      >
+        <img :src="item.image" :alt="item.companyimage" class="mr-3 h-auto w-12 rounded-full" />
         <div>
           <p>{{ item.company }}</p>
-          <p class="text-sm">{{ item.startDate }} {{item.endDate ? `a ${item.endDate}` : '| Actualidad' }} {{item.showDate ? ' | ' + calculateTime(item.startDate, item.endDate) : null}}</p>
+          <p class="text-sm">
+            {{ item.startDate }} {{ item.endDate ? `a ${item.endDate}` : "| Actualidad" }}
+            {{ item.showDate ? " | " + calculateTime(item.startDate, item.endDate) : null }}
+          </p>
         </div>
       </a>
-      <div class="relative border-l border-gray-200 dark:border-gray-700 mt-3 transition-colors duration-200">
-        <div class="mb-2 ml-4" v-for="(position, i) in item.positions" :key="i">
+      <div
+        class="relative mt-3 border-l border-gray-200 transition-colors duration-200 dark:border-gray-700"
+      >
+        <div class="mb-2 ml-4 mr-3" v-for="(position, i) in item.positions" :key="i">
           <div
-            class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700 transition-colors duration-200">
-          </div>
+            class="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 transition-colors duration-200 dark:border-gray-900 dark:bg-gray-700"
+          ></div>
           <div class="md:flex">
             <div class="md:basis-full">
-              <time class="text-gray-900 dark:text-white transition-colors duration-200">
-                <p class="underline text-lg font-bold">{{ position.name }}</p>
+              <time class="text-gray-900 transition-colors duration-200 dark:text-white">
+                <p class="text-lg font-bold underline">{{ position.name }}</p>
                 <p class="text-sm no-underline" v-if="position.showDate">
-                {{ position.startDate }} {{position.endDate ? `a ${position.endDate} |` : '| Actualidad |' }} {{calculateTime(position.startDate, position.endDate)}}
+                  {{ position.startDate }}
+                  {{ position.endDate ? `a ${position.endDate} |` : "| Actualidad |" }}
+                  {{ calculateTime(position.startDate, position.endDate) }}
                 </p>
               </time>
-              <ul class="mb-4 text-base font-normal list-disc	text-gray-500 dark:text-gray-400 ml-4 transition-colors duration-200">
+              <ul
+                class="mb-4 ml-4 list-disc text-base font-normal text-gray-500 transition-colors duration-200 dark:text-gray-400"
+              >
                 <li v-for="(responsability, index) in position.responsabilities" :key="index">
-                  {{responsability}}
+                  {{ responsability }}
                 </li>
               </ul>
             </div>
-            <div class="md:basis-1/4 flex flex-wrap items-center hidden">
+            <div class="flex hidden flex-wrap items-center md:basis-1/4">
               <p class="w-full text-center font-bold">Skills</p>
-              <span v-for="(skill, index) in position.skills" :key="index" class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 h-5 mb-2 md:mb-0 transition-colors duration-200">
-                {{skill}}
+              <span
+                v-for="(skill, index) in position.skills"
+                :key="index"
+                class="mr-2 mb-2 h-5 rounded bg-blue-100 px-2.5 py-0.5 text-xs font-semibold text-blue-800 transition-colors duration-200 dark:bg-blue-200 dark:text-blue-800 md:mb-0"
+              >
+                {{ skill }}
               </span>
             </div>
           </div>
@@ -47,23 +64,23 @@
   </ol>
 </template>
 <script lang="ts">
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 import experiences from "../utils/experiences.json";
 export default {
   data() {
     return {
-      experiences: experiences
-    }
+      experiences: experiences,
+    };
   },
   methods: {
-    calculateTime(startDate:string, endDate:string) {
+    calculateTime(startDate: string, endDate: string) {
       if (endDate) {
-        return dayjs(endDate).diff(startDate, 'month') + ' Meses'
+        return dayjs(endDate).diff(startDate, "month") + " Meses";
       } else {
-        const now = dayjs(new Date).format('YYYY/MM/DD')
-        return dayjs(now).diff(startDate, 'month') + ' Meses'
-      }      
-    }
-  }
-}
+        const now = dayjs(new Date()).format("YYYY/MM/DD");
+        return dayjs(now).diff(startDate, "month") + " Meses";
+      }
+    },
+  },
+};
 </script>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,130 +1,180 @@
 <template>
-  <h4 class="mb-3 text-center text-xl font-medium uppercase text-blue-600 md:text-2xl border-b-2 border-blue-300 dark:text-white dark:border-white flex justify-center">
-    <img class="dark:hidden mr-2 mb-1" src="../assets/icon-skills-black.png" alt="education" width="32">
-    <img class="hidden dark:block mr-2 mb-1" src="../assets/icon-skills-white.png" alt="education" width="32">
+  <h4
+    class="mb-3 flex justify-center border-b-2 border-blue-300 text-center text-xl font-medium uppercase text-blue-600 dark:border-white dark:text-white md:text-2xl"
+  >
+    <img
+      class="mr-2 mb-1 dark:hidden"
+      src="../assets/icon-skills-black.png"
+      alt="education"
+      width="32"
+    />
+    <img
+      class="mr-2 mb-1 hidden dark:block"
+      src="../assets/icon-skills-white.png"
+      alt="education"
+      width="32"
+    />
     habilidades
   </h4>
   <div class="border-b-2 border-blue-100 pb-3 dark:text-white">
-    <h4 class="text-center font-bold md:font-semibold md:text-left mb-2 text-lg underline md:no-underline">FrontEnd</h4>
+    <h4
+      class="mb-2 text-center text-lg font-bold underline md:text-left md:font-semibold md:no-underline"
+    >
+      FrontEnd
+    </h4>
     Javascript
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 80%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 85%"
+      ></div>
     </div>
     jQuery
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 60%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 60%"
+      ></div>
     </div>
     VueJs 2^ | NuxtJs
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 90%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 90%"
+      ></div>
     </div>
     ReactJs | Next.js
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 20%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 30%"
+      ></div>
     </div>
     Html | Css | Sass
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 90%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 90%"
+      ></div>
     </div>
     Bootstrap 3^
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 90%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 90%"
+      ></div>
     </div>
     Materializecss
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 70%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 70%"
+      ></div>
     </div>
     Tailwind CSS
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 50%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 50%"
+      ></div>
     </div>
     Cypress
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 50%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 50%"
+      ></div>
     </div>
   </div>
   <div class="border-b-2 border-blue-100 pb-3 dark:text-white">
-    <h4 class="text-center font-bold md:font-semibold md:text-left mb-2 text-lg underline md:no-underline mt-3">BackEnd</h4>
+    <h4
+      class="mb-2 mt-3 text-center text-lg font-bold underline md:text-left md:font-semibold md:no-underline"
+    >
+      BackEnd
+    </h4>
     Php
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-700 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 70%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-700 text-center font-medium leading-none text-white"
+        style="width: 80%"
+      ></div>
     </div>
     Laravel
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-700 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 80%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-700 text-center font-medium leading-none text-white"
+        style="width: 85%"
+      ></div>
     </div>
     NodeJs
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-700 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 30%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-700 text-center font-medium leading-none text-white"
+        style="width: 40%"
+      ></div>
     </div>
     MySQL
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-700 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 50%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-700 text-center font-medium leading-none text-white"
+        style="width: 60%"
+      ></div>
     </div>
     Oracle 11g
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-700 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 50%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-700 text-center font-medium leading-none text-white"
+        style="width: 60%"
+      ></div>
     </div>
     PostgresSQL
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-700 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 50%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-700 text-center font-medium leading-none text-white"
+        style="width: 60%"
+      ></div>
+    </div>
+    SQL Server
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-700 text-center font-medium leading-none text-white"
+        style="width: 20%"
+      ></div>
     </div>
   </div>
   <div class="border-b-2 border-blue-100 pb-3 dark:text-white">
-    <h4 class="text-center font-bold md:font-semibold md:text-left mb-2 text-lg underline md:no-underline mt-3">DevOps</h4>
+    <h4
+      class="mb-2 mt-3 text-center text-lg font-bold underline md:text-left md:font-semibold md:no-underline"
+    >
+      DevOps
+    </h4>
     Git|Git Flow|Bitbucket|GitLab|GitHub
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 80%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 80%"
+      ></div>
     </div>
     Nginx
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 50%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 60%"
+      ></div>
     </div>
     Ubuntu | Centos
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 50%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 60%"
+      ></div>
     </div>
     DigitalOcean
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
-      <div class="bg-blue-500 font-medium text-white text-center h-2.5 leading-none rounded-full w-50"
-        style="width: 50%">
-      </div>
+    <div class="mb-2 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+      <div
+        class="w-50 h-2.5 rounded-full bg-blue-500 text-center font-medium leading-none text-white"
+        style="width: 60%"
+      ></div>
     </div>
   </div>
 </template>
